3edcdb1a0cca48ed8564ba15c15f016306d27f41
David Adams <daveadams@gmail.com>
2022-12-01 08:01:13 -0600

    2022 Advent of Code! Day 01 part 1

---
+++ 2022/day01/part1/solution.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# declare -i sets a variable to be of integer type
+declare -i line elf=0 max=0
+
+# $elves is an array of integers
+declare -i -a elves
+
+while read line; do
+    if (( line == 0 )); then
+        if (( elves[$elf] > max )); then
+            max=${elves[elf]}
+        fi
+        elf+=1
+        continue
+    fi
+    elves[elf]+=$line
+done
+
+echo $max
