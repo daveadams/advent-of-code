92d16cc66bd318f254fc343be9d2c866d41fed69
David Adams <daveadams@gmail.com>
2022-12-06 16:21:37 -0600

    Update 2022d06 part2

--- 2022/day06/bash/part2.sh
+++ 2022/day06/bash/part2.sh
@@ -5,23 +5,19 @@ declare -i i c=0
 declare -i -r BUFFER_LENGTH=14
 
 while read -n1 ch; do
-    if [[ -z $ch ]]; then
-        continue
-    fi
-
     # rotate the characters through the buffer
     i=$(( c % BUFFER_LENGTH ))
     buffer[$i]=$ch
     c+=1
 
     # this isn't possible during the first BUFFER_LENGTH characters
-    if (( c < BUFFER_LENGTH )); then
-        continue
-    fi
+    (( c < BUFFER_LENGTH )) && continue
 
+    # compare all the combinations
     for (( x = 0; x < BUFFER_LENGTH - 1; x++ )); do
         for (( y = x + 1; y < BUFFER_LENGTH; y++ )); do
             if [[ ${buffer[$x]} == ${buffer[$y]} ]]; then
+                # continue the topmost loop
                 continue 3
             fi
         done
